78d5dd64a478384b293c671c6f93e5fdecc0bac7
W10\Ruben <rubenleis@gmail.com>
2016-11-26 14:59:09 +0100

    Completed with bonus

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+
+//BONUS IMPLEMENTED
+//https://github.com/jhu-ep-coursera/fullstack-course5/blob/master/assignments/assignment1/Assignment-1.md
 (function () {
 'use strict';
 
@@ -6,27 +9,41 @@ angular.module('LunchCheck', [])
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
-  $scope.name = "Ruben";
+
   $scope.message = "";
   $scope.checkFood = function() {
-    if ($scope.food) {
-        var totalFood = calculateNumberOfFoods($scope.food)
+    if ($scope.foodList) {
+        var totalFood = calculateNumberOfFoods($scope.foodList)
         if (totalFood <4) {
             $scope.message = "Enjoy!";
         } else {
             $scope.message = "Too much!"
         }
+        $scope.messageStyle = {color:'green'};
+        $scope.textboxStyle = {border:'solid 1px green'};
     } else {
         $scope.message = "Please enter data first";
+        $scope.messageStyle = {color:'red'};
+        $scope.textboxStyle = {border:'solid 1px red'};
+        
     }
   };
-    
+  
+   /*
+   * Count the items separated by , separator.
+   * Do NOT consider and empty item
+   */
   function calculateNumberOfFoods(string) {
     var foodArray = string.split(',');
-    var length = foodArray.length;
-    return length;
+    var arrayLength = foodArray.length;
+    var itemsCount=0;
+    for (var i = 0; i < arrayLength; i++) {
+        var food = foodArray[i];
+        if (food) {
+            itemsCount++;
+        }
+    }  
+    return itemsCount;
   }
 }
-    
-
 })();
